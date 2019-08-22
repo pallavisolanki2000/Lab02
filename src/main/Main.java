76c0d565cb7c424208f8c856e0210f09a2f587e3
pallavisolanki2000 <pallavi.solanki_cs18@gla.ac.in>
2019-08-22 15:14:57 +0530

    accept input from the user

--- src/main/Main.java
+++ src/main/Main.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String userName = "";
+        String userName = scanner.nextLine().trim();
         scanner.close();
 
     }
