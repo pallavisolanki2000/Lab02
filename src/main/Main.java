64cad2c7a5372ea71906ce76f24be1633958cdfd
pallavisolanki2000 <pallavi.solanki_cs18@gla.ac.in>
2019-08-22 15:13:46 +0530

    create object of scanner class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,6 @@
 package main;
+
+import java.util.Scanner;
 /**
  * @author pallavi
  * @version
@@ -6,6 +8,9 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String userName = "";
+        scanner.close();
+
     }
 }
