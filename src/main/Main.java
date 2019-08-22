d0fdb8c74cf398a5c21e43dadc240ceb1a88358f
pallavisolanki2000 <pallavi.solanki_cs18@gla.ac.in>
2019-08-22 15:00:32 +0530

    add file Main.java

---
+++ src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+/**
+ * @author pallavi
+ * @version
+ */
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = Scanner.nextLine();
+        System.out.println("Welcome," + s);
+    }
+}
